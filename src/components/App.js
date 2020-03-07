0fd5e50bccfbed2ea80f3010ab7032da1865d8a2
Paris Ward <wardparis.pw@gmail.com>
2020-03-06 16:02:12 -0800

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+
+class App extends React.Component {
+  render() {
+    return(
+      <div className="container">
+        <BrowserRouter>
+          <div className="container">
+            
+          </div>
+        </BrowserRouter>
+      </div>
+    )
+  }
+}
+
+export default App;
